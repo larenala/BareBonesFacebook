3c9a60183caef271a51d460472abff4d8b2080d0
Laura <laura.reivinen@gmail.com>
2019-05-02 13:11:56 +0300

    ensimmäinen versio

---
+++ src/main/java/projekti/AccountController.java
@@ -0,0 +1,60 @@
+
+package projekti;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class AccountController {
+    
+    @Autowired
+    AccountRepository accountRepository;
+
+    @Autowired
+    PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+    
+  
+    
+    @GetMapping("/register")
+    public String getRegisterPage() {
+        return "register";
+    }
+    
+    @PostMapping("/register")
+    public String registerNewAccount(@RequestParam String username, @RequestParam String name,
+            @RequestParam String password, @RequestParam String publicName) {
+        if (accountRepository.findByUsername(username) != null) {
+            return "redirect:/register";
+        }
+        Account a = new Account(username, name, passwordEncoder.encode(password), publicName);
+        accountRepository.save(a);
+        return "redirect:/login";   
+    }
+    
+    
+    @GetMapping("/profile")
+    public String showProfile(Model model) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        String username = auth.getName();
+        model.addAttribute("person", accountRepository.findByUsername(username));
+        return "profile";
+    }
+    
+    @GetMapping("/index")
+    public String getIndex(Model model) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        String username = auth.getName();
+        model.addAttribute("name", accountRepository.findByUsername(username).getName());
+        return "index";
+    }
+}
---
+++ src/main/java/projekti/SecurityConfiguration.java
@@ -0,0 +1,54 @@
+
+package projekti;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Profile("default")
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+    
+    @Autowired
+    private UserDetailsService userDetailsService;
+    
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http.csrf().disable();
+        // sallitaan framejen käyttö
+        http.headers().frameOptions().sameOrigin();
+        
+        http.authorizeRequests()
+                .antMatchers("/").permitAll()
+                .antMatchers("/register","/register/**").permitAll()
+                .antMatchers("/h2-console","/h2-console/**").permitAll()
+                .anyRequest().authenticated();
+        
+        http
+                .formLogin().permitAll()
+                .defaultSuccessUrl("/index",true)
+                .and()
+                .logout().permitAll()
+                .logoutSuccessUrl("/register");
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
